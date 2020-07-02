fffe6d22a1de1894cda378d6201ecd291cc4225e
Chang'anxiaoxiang <1925879492@qq.com>
2020-07-02 08:46:06 +0800

    简单爬虫获取快递信息并邮箱发送，记得填写收件人和发件人以及你的QQ邮箱SMTP授权码

---
+++ 快递查询.py
@@ -0,0 +1,63 @@
+﻿import requests,bs4,re,time
+
+from email.mime.text import MIMEText
+from email.header import Header
+from smtplib import SMTP_SSL
+
+now= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0'}
+
+number = (input('请输入快递单号'))
+sort = input(
+    '请输入快递服务商。顺丰：sfexpress，韵达：yunda，申通：sto，圆通：yto，中通：zto，EMS：ems，邮政：chinapost（请输入对应英文！）')
+
+
+def get_url(code, id):
+    url = 'http://m.46644.com/express/result.php?typetxt=%D6%D0%CD%A8&type=' + \
+        code + '&number=' + id
+    return url
+
+
+url = get_url(sort, number)
+print(url)
+
+if __name__ == "__main__":
+    while True:
+        kuaidi = []
+        response = requests.get(url, headers=headers)
+        response.encoding = 'gb18030'
+        response = response.text
+        soup = bs4.BeautifulSoup(response, 'html.parser', from_encoding="utf8")
+        for i in soup.findAll(name='div', attrs={'class': 'icontent'}):
+            kuaidi.append(i.get_text())
+            print(i.get_text())
+
+        host_server = 'smtp.qq.com'
+        # pwd为qq邮箱的授权码
+        pwd = '16x* Your smtp password'
+        # 发件人的邮箱
+        sender_qq_mail = 'sender@qq.com'
+        # 收件人邮箱
+        receiver = 'receiver's E-mail address'
+
+        # 邮件的正文内容
+        mail_content = '\n'.join(kuaidi[::-1])+'\n\n发送于：'+now
+        # 邮件标题
+        mail_title = '快递追踪'
+
+        # ssl登录
+        smtp = SMTP_SSL(host_server)
+        # set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+        smtp.set_debuglevel(0)
+        smtp.ehlo(host_server)
+        smtp.login(sender_qq, pwd)
+
+        msg = MIMEText(mail_content, "plain", 'utf-8')
+        msg["Subject"] = Header(mail_title, 'utf-8')
+        msg["From"] = sender_qq_mail
+        msg["To"] = receiver
+        smtp.sendmail(sender_qq_mail, receiver, msg.as_string())
+        smtp.quit()
+        time.sleep(3600)
---
+++ 获取ip地址并邮件发送.py
@@ -0,0 +1,46 @@
+﻿from email.mime.text import MIMEText
+from email.header import Header
+from smtplib import SMTP_SSL
+import socket,time,requests,re
+
+headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0'} 
+
+#qq邮箱smtp服务器
+host_server = 'smtp.qq.com'
+#sender_qq为发件人的qq邮箱号码
+sender_qq = 'Your QQ email ID xxxxxxxxx@qq.com'
+#pwd为qq邮箱的授权码
+pwd = 'Your SMTP password ****************'
+#收件人邮箱
+receiver = 'receiver's E-mail address'
+def send(ip):
+    print (ip[0])
+    hostname = socket.gethostname()
+
+    now= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+    #邮件的正文内容
+    mail_content = hostname+'\nIP address：'+ip[0]+'\nTime：'+now
+    #邮件标题
+    mail_title = 'IPv4 address！'
+
+    #ssl登录
+    smtp = SMTP_SSL(host_server)
+    #set_debuglevel()是用来调试的。参数值为1表示开启调试模式，参数值为0关闭调试模式
+    smtp.set_debuglevel(0)
+    smtp.ehlo(host_server)
+    smtp.login(sender_qq, pwd)
+
+    msg = MIMEText(mail_content, "plain", 'utf-8')
+    msg["Subject"] = Header(mail_title, 'utf-8')
+    msg["From"] = sender_qq_mail
+    msg["To"] = receiver
+    smtp.sendmail(sender_qq_mail, receiver, msg.as_string())
+    smtp.quit()
+
+while True:
+    html_text = requests.get("https://ip.cn/",headers=headers).text
+    ip = re.findall("<code>(.*?)</code>", html_text) 
+    send(ip)
+    time.sleep(3600)
+    
