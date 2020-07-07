526b8c82005794332dab5e2f992f5f3962a82322
Chang'anxiaoxiang <1925879492@qq.com>
2020-07-07 12:29:14 +0800

    I want to send it to my raspberry pi.

---
+++ 精神小火on ubuntu.py
@@ -0,0 +1,8 @@
+import pyautogui,time,socket
+
+while True:
+    now= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    hostname = socket.gethostname()
+    pyautogui.click(797,657,button='left')
+    pyautogui.typewrite('Send at:'+now +'  on'+hostname+'  for weichijunliaochiyan(wobuhuifanyi)')
+    time.sleep(600)
