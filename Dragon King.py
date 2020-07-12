10191ae66be5c100f24482375d07646ab7eb1756
Chang'anxiaoxiang <1925879492@qq.com>
2020-07-12 23:52:36 +0800

    删除了一些东西

--- Dragon King.py
+++
@@ -1,8 +0,0 @@
-import pyautogui,time
-while True:
-    for x in range(200):
-        pyautogui.click(797,657,button='left')
-        pyautogui.typewrite('జ్ఞ ా رً ॣ'+'longwang!'+str(x))
-        pyautogui.typewrite(['enter'])
-        time.sleep(0.1)
-    time.sleep(600)
