c877f602f280941ae48faed1bb0c4bc542a0245f
Chang'anxiaoxiang <1925879492@qq.com>
2020-07-12 23:52:36 +0800

    删除了一些东西

--- 精神小火on ubuntu.py
+++
@@ -1,9 +0,0 @@
-import pyautogui,time,socket
-
-while True:
-    now= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-    hostname = socket.gethostname()
-    pyautogui.click(797,657,button='left')
-    pyautogui.typewrite('Send at:'+now +'  on'+hostname+'  for weichijunliaochiyan(wobuhuifanyi)')
-    pyautogui.typewrite(['enter'])
-    time.sleep(600)
